bf1ba826b755a8856bb8311a702d7517604cb1df
Alexander Jose Gonzalez solorzano <71440690+Alexmetalhead@users.noreply.github.com>
2025-02-16 18:49:16 -0300

    #7DaysOfCode - Lógica JS 2/7: Variables
    ¿Sabes cuando te registras en un site y, justo después, al iniciar sesión, te llama por tu nombre? Eso es lo que harás en el desafío de hoy.
    
    Cuando creas un sistema, site o aplicación, es común querer agregar algunos toques personalizados para hacer la experiencia en tu aplicación más rica y dinámica.
    
    Hacer esto mediante programación puede no ser una tarea fácil. Dependiendo del nivel de personalización que quieras implementar, la cantidad de código que necesitarás escribir puede ser inmensa.
    
    Pero, por supuesto, puedes comenzar de una manera más sencilla. Para ello, lo importante es entender cómo capturar y almacenar valores dentro de variables. ¡Y en eso te voy a ayudar hoy!
    
    Las variables son los bloques básicos de construcción de cualquier sistema y son esenciales para procesar cualquier tipo de información, ya sea de una persona que ha iniciado sesión en el sistema o incluso para mostrar detalles de productos en un catálogo de comercio electrónico.
    
    Por eso, hoy te voy a enseñar a desarrollar un programa simulando una de esas aplicaciones. Debe pedir al usuario responder 3 preguntas:
    
    - ¿Cuál es tu nombre?
    - ¿Cuántos años tienes?
    - ¿Qué lenguaje de programación estás estudiando?
    
    A medida que se hagan las preguntas, la persona que esté usando el programa debe responder cada una de ellas.
    
    Al final, el sistema mostrará el mensaje:
    
    "Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]!"
    
    Observa que cada información entre [ ] es una de las respuestas dadas por la persona.

---
+++ 7DaysOfCode - day2/app.js
@@ -0,0 +1,8 @@
+const nombreUsuario = prompt("¿Cuál es tu nombre?");
+console.log(nombreUsuario);
+const anosUsuario = prompt("¿Cuántos años tienes?");
+console.log(anosUsuario);
+const lenguaje =prompt("¿Qué lenguaje de programación estás estudiando?");
+console.log(lenguaje);
+const mensajeSistema =`Hola ${nombreUsuario}, tienes ${anosUsuario} años y ya estás aprendiendo ${lenguaje}!`;
+alert(mensajeSistema);
