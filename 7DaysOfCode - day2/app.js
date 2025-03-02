df7bea3ca28c883d697d74388964a4b1a38fd835
Alexander Jose Gonzalez solorzano <71440690+Alexmetalhead@users.noreply.github.com>
2025-03-02 13:37:23 -0300

    #7DaysOfCode - Lógica JS 2/7: Variables
    #7DaysOfCode - Lógica JS 2/7: Variables

--- 7DaysOfCode - day2/app.js
+++ 7DaysOfCode - day2/app.js
@@ -1,3 +1,4 @@
+window.addEventListener("DOMContentLoaded", (event) => {
 const nombreUsuario = prompt("¿Cuál es tu nombre?");
 console.log(nombreUsuario);
 const anosUsuario = prompt("¿Cuántos años tienes?");
@@ -6,3 +7,5 @@ const lenguaje =prompt("¿Qué lenguaje de programación estás estudiando?");
 console.log(lenguaje);
 const mensajeSistema =`Hola ${nombreUsuario}, tienes ${anosUsuario} años y ya estás aprendiendo ${lenguaje}!`;
 alert(mensajeSistema);
+})
+
